03c4757165767342851c87ff44664347b919d348
ahmedrehane <rehaneahmed49@gmail.com>
2023-10-18 11:30:09 +0100

    confirme

--- index.js
+++ index.js
@@ -1,2 +1,28 @@
 alert('hello')
 
+let sentence = `short sentence delivers a powerful message with concise and effective language.
+ short sentence is a shanghai-based womenswear brand founded in 2015.
+  the brand pays homage to modern women through a collection of comfortable and timeless essentials.
+   commonplace and banal elements in everyday life inspire short sentence to design understated yet
+    evocative pieces  and pursue life-affirming fashion.`
+
+let charactereCounter = 0 ;
+
+for(let i = 0 ;i < sentence.length;i++){
+    if(sentence[i] != " "){
+        charactereCounter++
+        
+    }
+}
+console.log(charactereCounter)
+
+
+let wordcounter = 0
+
+for(let i = 0 ;i< sentence.length;i++){
+    if(sentence[i] == " "){
+        wordcounter++
+    }
+    
+}
+console.log(wordcounter)
